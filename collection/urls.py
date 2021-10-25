5900583c88124e0a4702bf924e302672a53bf336
UzziD <uzzisoft@gmail.com>
2021-10-25 16:26:15 -0600

    create barebones addentry with no functionality

--- collection/urls.py
+++ collection/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addEntry', views.addEntry, name='addEntry')
 
 ]
--- collection/views.py
+++ collection/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("The item collection goes here")
+def addEntry(request):
+    return HttpResponse(str(request))
