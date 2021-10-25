02aa012aaafc185e7a737adf44956c87d136c621
UzziD <uzzisoft@gmail.com>
2021-10-25 17:11:21 -0600

    attempt to resolve type error in addentry and changed index in views

--- collection/views.py
+++ collection/views.py
@@ -3,10 +3,8 @@ from django.http import HttpResponse
 from .models import entry
 # Create your views here.
 def index(request):
-    allentries = entry.objects
-    output = '\n'.join(allentries) #lists every entry in the collection
-    return HttpResponse(output)
-def addEntry(request, userEntry: str):
+    return HttpResponse(entry.objects.all())
+def addEntry(request, userEntry):
     #clean = cleaner(user_entry)
     itemizedEntry = userEntry.split(',')
     entrant = entry.objects.create(item = itemizedEntry[0], count = itemizedEntry[1], author = itemizedEntry[2])
