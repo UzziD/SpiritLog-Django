fbb59b2835a2798626ea580ba7289920c8896860
UzziD <uzzisoft@gmail.com>
2021-10-25 16:37:59 -0600

    added collection app to settings

--- spiritlog/settings.py
+++ spiritlog/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'collection.apps.CollectionConfig',
 ]
 
 MIDDLEWARE = [
