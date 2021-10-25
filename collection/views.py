708ba1fbf70c804f0bb9754bd5ffeee4a4c79808
UzziD <uzzisoft@gmail.com>
2021-10-25 17:05:54 -0600

    created list view for index and added barebone functionality to addentry

--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import entry
 # Create your views here.
 def index(request):
-    return HttpResponse("The item collection goes here")
-def addEntry(request, entry):
-    return HttpResponse(str(entry))
+    allentries = entry.objects
+    output = '\n'.join(allentries) #lists every entry in the collection
+    return HttpResponse(output)
+def addEntry(request, userEntry: str):
+    #clean = cleaner(user_entry)
+    itemizedEntry = userEntry.split(',')
+    entrant = entry.objects.create(item = itemizedEntry[0], count = itemizedEntry[1], author = itemizedEntry[2])
+    return HttpResponse(str(entrant))
