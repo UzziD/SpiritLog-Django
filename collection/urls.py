8d6a2a36f1802386e6ae601772310d5fed26e6fc
UzziD <uzzisoft@gmail.com>
2021-10-25 14:45:08 -0600

    added index for collection

---
+++ collection/urls.py
@@ -0,0 +1,9 @@
+from django.urls import __path__
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+]
--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("The item collection goes here")
