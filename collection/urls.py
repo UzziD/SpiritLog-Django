577c71a40f76c0db8c454bb902b714f005068b19
UzziD <uzzisoft@gmail.com>
2021-10-25 16:30:22 -0600

    create barebones addentry with echo

--- collection/urls.py
+++ collection/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('addEntry', views.addEntry, name='addEntry')
+    path('addEntry/<str:entry>/', views.addEntry, name='addEntry')
 
 ]
--- collection/views.py
+++ collection/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("The item collection goes here")
-def addEntry(request):
-    return HttpResponse(str(request))
+def addEntry(request, entry):
+    return HttpResponse(str(entry))
