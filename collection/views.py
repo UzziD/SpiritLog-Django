e39eca6abf163dcecd02ea191606812672e7a4e4
UzziD <uzzisoft@gmail.com>
2021-10-25 17:16:51 -0600

    attempt to resolve issues with userEntry and registered entry model into admin

--- collection/views.py
+++ collection/views.py
@@ -4,8 +4,9 @@ from .models import entry
 # Create your views here.
 def index(request):
     return HttpResponse(entry.objects.all())
+
 def addEntry(request, userEntry):
     #clean = cleaner(user_entry)
     itemizedEntry = userEntry.split(',')
     entrant = entry.objects.create(item = itemizedEntry[0], count = itemizedEntry[1], author = itemizedEntry[2])
-    return HttpResponse(str(entrant))
+    return HttpResponse(userEntry)
