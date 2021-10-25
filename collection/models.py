d44d7f48df3ae5ab383d9645072d9a8cf57abdb6
UzziD <uzzisoft@gmail.com>
2021-10-25 16:13:13 -0600

    create model for collection entry

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class entry(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    item = models.CharField(max_length=100)
+    count = models.PositiveIntegerField()
+    author = models.CharField(max_length=255)
+
+    @property
+    def __str__(self):
+        return "%s: %d %s" % (self.author, self.count, self.item)
