b53f46e8c8ba39f9f15af3231989efc7c671d4ec
UzziD <uzzisoft@gmail.com>
2021-10-26 16:47:55 -0600

    added raredroptableitems to visible tables in admin page

--- collection/admin.py
+++ collection/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Entry
-
+from .models import RareDropTableItems
 admin.site.register(Entry)
+admin.site.register(RareDropTableItems)
