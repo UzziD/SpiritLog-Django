d180a8e99c9d70b6e832bd295106d5e3789ba1f2
UzziD <uzzisoft@gmail.com>
2021-10-26 16:38:55 -0600

    added model for aliases and script for loading csv to db

---
+++ collection/management/commands/loadcsv.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from collection.models import RareDropTableItems
+import csv
+
+class Command(BaseCommand):
+    help = "Preloads db with data from csv"
+
+    def add_arguments(self, parser):
+        parser.add_arguments('path', type='str', help='Indicates the path to the csv file')
+
+    def handle(self, *args, **options):
+        path = options['path']
+        with open(path) as f:
+            reader = csv.reader(f)
+            next(reader, None) #use this to skip header
+            for row in reader:
+                _, created = RareDropTableItems.objects.get_or_create(
+                    item=row[0],
+                    itemAlias=row[1],
+                    )
+                # creates a tuple of the new object or
+                # current object and a boolean of if it was created        
--- collection/models.py
+++ collection/models.py
@@ -9,3 +9,11 @@ class Entry(models.Model):
     @property
     def __str__(self):
         return "%s: %d %s" % (self.author, self.count, self.item)
+
+class RareDropTableItems(models.Model):
+    item = models.CharField(max_length=100)
+    itemAlias = models.CharField(max_length=100, primary_key=True)
+
+    @property
+    def __str__(self):
+        return "%s" % (self.itemAlias)
