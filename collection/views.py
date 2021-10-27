17b3ccaa676c2800aa36a35676b62aaf00766333
UzziD <uzzisoft@gmail.com>
2021-10-26 18:11:22 -0600

    changed filter argument in check for addentry

--- collection/views.py
+++ collection/views.py
@@ -15,7 +15,7 @@ def addEntry(request):
     if request.method == 'POST':
         form = EntryForm(request.POST)
         if form.is_valid():
-            item = RareDropTableItems.objects.filter(itemAlias=form['item']).first()  #grab item name based on user's input item
+            item = RareDropTableItems.objects.filter(itemAlias=form.item).first()  #grab item name based on user's input item
             itemEntry = form.save(commit=False)                     #save the non validated form to change to proper item name
             itemEntry.item = item
             itemEntry.save()
