46c7b5176786b4a1a18da9f0dec8dcb925b09c3f
UzziD <uzzisoft@gmail.com>
2021-10-26 18:52:05 -0600

    removed property decorator from models

--- collection/models.py
+++ collection/models.py
@@ -6,7 +6,6 @@ class Entry(models.Model):
     count = models.PositiveIntegerField()
     author = models.CharField(max_length=255)
 
-    @property
     def __str__(self):
         return "%s: %d %s" % (self.author, self.count, self.item)
 
@@ -14,6 +13,5 @@ class RareDropTableItems(models.Model):
     item = models.CharField(max_length=100)
     itemAlias = models.CharField(max_length=100, primary_key=True)
 
-    @property
     def __str__(self):
         return "%s" % (self.item)
