0aafb7aec1f8bc09ddfb9f7e9198f9d368315e25
UzziD <uzzisoft@gmail.com>
2021-10-28 21:20:00 -0600

    add comma in settings

--- spiritlog/settings.py
+++ spiritlog/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'collection.apps.CollectionConfig'
+    'collection.apps.CollectionConfig',
     'homepage',
 ]
 
