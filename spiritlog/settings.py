e2f15e4812bb7bda487efcacf1af4d51cac2344e
UzziD <uzzisoft@gmail.com>
2021-10-28 21:15:31 -0600

    configured homepage applet

--- spiritlog/settings.py
+++ spiritlog/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'collection.apps.CollectionConfig',
+    'collection.apps.CollectionConfig'
+    'homepage',
 ]
 
 MIDDLEWARE = [
--- spiritlog/urls.py
+++ spiritlog/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', include('homepage.urls')),
     path('admin/', admin.site.urls),
     path('collection/', include('collection.urls'))
 ]
